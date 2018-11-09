d55068df29e0fa79da078ea45547df875ee59b19
byungtak <>
2018-11-10 04:03:15 +0900

    update tmap : get current location, set range, design

--- app/src/main/java/com/empathy/empathy_android/ui/tmap/MapActivity.kt
+++ app/src/main/java/com/empathy/empathy_android/ui/tmap/MapActivity.kt
@@ -1,14 +1,51 @@
 package com.empathy.empathy_android.ui.tmap
 
+import android.Manifest
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.ListView
+import android.widget.PopupWindow
+import androidx.core.content.res.ResourcesCompat
 import com.empathy.empathy_android.BaseActivity
 import com.empathy.empathy_android.R
 import com.empathy.empathy_android.ui.login.MapViewModel
 import com.skt.Tmap.TMapView
 import kotlinx.android.synthetic.main.activity_map.*
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.Manifest.permission
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.location.Location
+import android.util.Log
+import com.skt.Tmap.TMapCircle
+import com.skt.Tmap.TMapGpsManager
+import com.skt.Tmap.TMapGpsManager.GPS_PROVIDER
+import com.skt.Tmap.TMapGpsManager.NETWORK_PROVIDER
+import com.skt.Tmap.TMapPoint
+import com.tbruyelle.rxpermissions2.RxPermissions
 
 
-internal class MapActivity: BaseActivity<MapViewModel.ViewModel>() {
+internal class MapActivity: BaseActivity<MapViewModel.ViewModel>(), TMapGpsManager.onLocationChangedCallback {
+
+    companion object {
+        private var circleId = 0
+    }
+
+    private val popupWindow = PopupWindow()
+
+    private val spinnerAdapter = RangeAdapter()
+
+    private val tMapView by lazy {
+        TMapView(this)
+    }
+
+    private val tmapGpsManager by lazy {
+        TMapGpsManager(this)
+    }
 
     override fun getLayoutRes(): Int = R.layout.activity_map
     override fun getViewModel(): Class<MapViewModel.ViewModel> = MapViewModel.ViewModel::class.java
@@ -16,10 +53,107 @@ internal class MapActivity: BaseActivity<MapViewModel.ViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        tmap_container.addView(
-                TMapView(this).apply {
-                    setSKTMapApiKey("b5be9a2e-d454-4177-8912-1d2c1cbee391")
-                }
+        initializeView()
+        initializeListener()
+    }
+
+    override fun onLocationChange(location: Location?) {
+        tMapView.setLocationPoint(location?.longitude!!, location.latitude)
+        
+        drawRangeCircle(location.longitude, location.latitude)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        tmapGpsManager.CloseGps()
+    }
+
+    private fun initializeView() {
+        setSupportActionBar(toolbar)
+        setRangePopupWindow()
+        setRxPermission()
+    }
+
+    private fun setRangePopupWindow() {
+        val rangeListView = ListView(this).apply {
+            adapter       = spinnerAdapter
+            dividerHeight = 0
+        }
+
+        popupWindow.apply {
+            contentView        = rangeListView
+            height             = WindowManager.LayoutParams.WRAP_CONTENT
+            width              = WindowManager.LayoutParams.WRAP_CONTENT
+            isFocusable        = true
+            isOutsideTouchable = true
+            animationStyle     = android.R.style.Animation_Dialog
+
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dismiss()
+        }
+    }
+
+    private fun setRxPermission() {
+        RxPermissions(this).run {
+            request(Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    .subscribe { granted ->
+                        if(granted) {
+                            setTmap()
+
+                            return@subscribe
+                        } else {
+                            return@subscribe
+                        }
+                    }
+        }
+    }
+
+    private fun setTmap() {
+        with(t_map_container) {
+            addView(tMapView.apply {
+                zoomLevel = 14
+
+                setSKTMapApiKey("b5be9a2e-d454-4177-8912-1d2c1cbee391")
+                setIconVisibility(true)
+                setTrackingMode(true)
+                setSightVisible(true)
+            })
+        }
+
+        tmapGpsManager.provider = NETWORK_PROVIDER
+        tmapGpsManager.minTime = 1000
+        tmapGpsManager.minDistance = 5F
+        tmapGpsManager.OpenGps()
+    }
+
+    private fun drawRangeCircle(longitude: Double, latitude: Double) {
+        val tmapCircle = TMapCircle().apply {
+            radius = 1000.0
+            lineColor = Color.BLUE
+            areaColor = Color.GRAY
+            areaAlpha = 100
+            circleWidth = 3.toFloat()
+            centerPoint = TMapPoint(latitude, longitude)
+        }
+
+        tMapView.addTMapCircle(
+                String.format("circle%d", circleId++),
+                tmapCircle
         )
     }
+
+    private fun initializeListener() {
+        search_range.setOnClickListener {
+
+        }
+
+        spinnerAdapter.setRangeItemClickListener(listener = object : RangeAdapter.RangeItemClickListener {
+            override fun onRangeItemClicked(range: String) {
+
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/empathy/empathy_android/ui/tmap/RangeAdapter.kt
+++ app/src/main/java/com/empathy/empathy_android/ui/tmap/RangeAdapter.kt
@@ -1,2 +1,63 @@
-package com.empathy.empathy_android.ui.tmap 
+package com.empathy.empathy_android.ui.tmap
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.empathy.empathy_android.R
+
+internal class RangeAdapter : BaseAdapter() {
+
+    interface RangeItemClickListener {
+        fun onRangeItemClicked(range: String)
+    }
+
+    private val ranges = listOf("100m", "300m", "500m", "1000m")
+
+    private var listener: RangeItemClickListener? = null
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val viewHolder: ViewHolder
+
+        if(convertView == null) {
+            view       = LayoutInflater.from(parent?.context).inflate(R.layout.layout_t_map_search_range, parent, false)
+            viewHolder = ViewHolder(view)
+
+            view.tag = viewHolder
+        } else {
+            view       = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
+        val range = ranges[position]
+
+        with(viewHolder) {
+            rangeTitle.text = range
+
+            rangeContainer.setOnClickListener {
+                listener?.onRangeItemClicked(range)
+            }
+        }
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any = ranges[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getCount(): Int = ranges.count()
+
+    fun setRangeItemClickListener(listener: RangeItemClickListener) {
+        this.listener = listener
+    }
+
+    private class ViewHolder(convertView: View) {
+        val rangeContainer = convertView.findViewById<LinearLayout>(R.id.range_container)
+        val rangeTitle     = convertView.findViewById<TextView>(R.id.range)
+    }
+
+}
