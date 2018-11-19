a2e5b765c4d54a1dd25189554aec435a27129865
byungtak <>
2018-11-19 18:45:58 +0900

    updating design on feeddetail activity

--- app/src/main/java/com/empathy/empathy_android/ui/feeddetail/FeedDetailActivity.kt
+++ app/src/main/java/com/empathy/empathy_android/ui/feeddetail/FeedDetailActivity.kt
@@ -1,7 +1,19 @@
 package com.empathy.empathy_android.ui.feeddetail
 
+import android.os.Bundle
 import com.empathy.empathy_android.BaseActivity
+import com.empathy.empathy_android.R
 
 
 internal class FeedDetailActivity: BaseActivity<FeedDetailViewModel.ViewModel>() {
+
+
+    override fun getLayoutRes(): Int = R.layout.activity_feed_detail
+    override fun getViewModel(): Class<FeedDetailViewModel.ViewModel> = FeedDetailViewModel.ViewModel::class.java
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
 }
--- app/src/main/java/com/empathy/empathy_android/ui/feeddetail/FeedDetailViewModel.kt
+++ app/src/main/java/com/empathy/empathy_android/ui/feeddetail/FeedDetailViewModel.kt
@@ -1,11 +1,17 @@
 package com.empathy.empathy_android.ui.feeddetail
 
 import com.empathy.empathy_android.BaseViewModel
+import javax.inject.Inject
 
 internal interface FeedDetailViewModel {
 
 
-    class ViewModel: BaseViewModel() {
+    class ViewModel @Inject constructor(
 
+    ): BaseViewModel() {
+
+        init {
+
+        }
     }
 }
