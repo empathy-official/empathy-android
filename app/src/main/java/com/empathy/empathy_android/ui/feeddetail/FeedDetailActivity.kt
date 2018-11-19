cb833695ac2d7c1233a5f4a19444121f52bdb396
Tak <tak8997@nate.com>
2018-11-20 00:22:35 +0900

    update design on deed activity

--- app/src/main/java/com/empathy/empathy_android/ui/feeddetail/FeedDetailActivity.kt
+++ app/src/main/java/com/empathy/empathy_android/ui/feeddetail/FeedDetailActivity.kt
@@ -7,6 +7,10 @@ import com.empathy.empathy_android.R
 
 internal class FeedDetailActivity: BaseActivity<FeedDetailViewModel.ViewModel>() {
 
+    companion object {
+        const val EXTRA_KEY_FEED_TYPE_MY_FEED    = "extra_key_feed_type_my_feed"
+        const val EXTRA_KEY_FEED_TYPE_OTHER_FEED = "extra_key_feed_type_other_feed"
+    }
 
     override fun getLayoutRes(): Int = R.layout.activity_feed_detail
     override fun getViewModel(): Class<FeedDetailViewModel.ViewModel> = FeedDetailViewModel.ViewModel::class.java
